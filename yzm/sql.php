cbc54ba03665f8ec19c90537baa589ec70e481bf
DHD0303 <31568906+DHD0303@users.noreply.github.com>
2018-06-08 23:21:38 +0800

    Update sql.php

--- yzm/sql.php
+++ yzm/sql.php
@@ -5,5 +5,6 @@
  * Date: 2018/6/8
  * Time: 22:44
  */
-$conn = new PDO("mysql:host=127.0.0.1;dbname=test", 'root', 'root');
+$conn = new PDO("mysql:host=127.0.0.1;dbname=test", 'root', '********
+');
 $conn->query('set names utf-8');
